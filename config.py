b7861269d2ea8122f295311db23de6195d851b69
Elesh-Norn <kercyan@live.fr>
2019-10-01 15:09:48 +0200

    Part 9: Pagination system

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['fakemail@fake.com']
+    POSTS_PER_PAGE = 25
