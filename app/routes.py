d7a2bf834b1abd9addcb0f5b8fba29c8f65d2bb1
Elesh-Norn <kercyan@live.fr>
2019-10-01 14:12:38 +0200

    Part 9: Submission of blog post

--- app/routes.py
+++ app/routes.py
@@ -5,20 +5,25 @@ from flask_login import current_user, login_user, logout_user
 from flask_login import login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.models import User
+from app.models import User, Post
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import PostForm
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {"username": "Daphnée"}
-    posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
-    ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form,
+                           posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -135,3 +140,11 @@ def unfollow(username):
     db.session.commit()
     flash("You are not following {}".format(username))
     return redirect(url_for('user', username=username))
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html',
+                           title='Explore',
+                           posts=posts)
