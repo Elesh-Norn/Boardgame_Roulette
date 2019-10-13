9904f81b77c766604496df8933c55bd29c7f802f
Elesh-Norn <kercyan@live.fr>
2019-10-13 19:28:17 +0200

    Reset emails

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 from flask_login import UserMixin
+from time import time
+import jwt
+from app import app
 
 
 @login.user_loader
@@ -65,7 +68,19 @@ class User(UserMixin, db.Model):
         return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
             digest, size
         )
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password.txt': self.id, 'exp':time()+expires_in},
+             app.config['SECRET_KEY'], algorithm="HS256").decode("utf-8")
 
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config["SECRET_KEY"],
+                            algorithms=['HS256'])['reset_password.txt']
+        except:
+            return
+        return User.query.get(id)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
