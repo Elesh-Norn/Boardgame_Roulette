1423c09dc1d6fe618a8ab28ba2dcf944f7c3fbf1
Elesh-Norn <kercyan@live.fr>
2020-03-20 19:04:24 +0100

    corrected html bug with the select field on random.html

--- app/schema.py
+++ app/schema.py
@@ -1,15 +1,10 @@
 from marshmallow import Schema, fields, validate
 
 
-def no_string_none(x):
-    if x == "None":
-        return False
-    return True
-
 
 class SearchSchema(Schema):
     name = fields.Str()
     player_number = fields.Integer()
     genre = fields.Str(validate=validate.Length(1, 10))
-    difficulty = fields.Str(validate=no_string_none)
+    difficulty = fields.Str()
     time = fields.Integer()
